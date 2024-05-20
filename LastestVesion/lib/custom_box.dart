31bc4ebb1432517044b444024751cf646d080280
TO <82508487+TT665cc@users.noreply.github.com>
2024-05-20 19:14:07 +0200

    Add files via upload

--- LastestVesion/lib/custom_box.dart
+++ LastestVesion/lib/custom_box.dart
@@ -4,9 +4,9 @@ class CustomBox extends StatelessWidget {
   final String title;
   final String description;
   final String imagePath;
-  final int distance;
-  final int altitude;
-  final int speed;
+  final double distance;
+  final double altitude;
+  final double speed;
 
   const CustomBox({
     Key? key,
@@ -23,50 +23,34 @@ class CustomBox extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.all(16.0),
       decoration: BoxDecoration(
-        color: Colors.grey[200],
+        color: Colors.white,
         borderRadius: BorderRadius.circular(10.0),
         boxShadow: [
           BoxShadow(
             color: Colors.grey.withOpacity(0.5),
             spreadRadius: 5,
             blurRadius: 7,
-            offset: const Offset(0, 3),
+            offset: Offset(0, 3), // changes position of shadow
           ),
         ],
       ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
         children: [
           Text(
             title,
-            style: Theme.of(context).textTheme.headlineSmall?.copyWith(
-              fontWeight: FontWeight.bold,
+            style: TextStyle(
+              fontFamily: 'Poppins'
             ),
           ),
           const SizedBox(height: 8.0),
-          Text(
-            description,
-            style: Theme.of(context).textTheme.bodyMedium,
-          ),
+          Text(description),
           const SizedBox(height: 8.0),
-          Row(
-            children: [
-              Image.asset('assets/images/desc1.png', height: 16),
-              const SizedBox(width: 4),
-              Text('$distance km', style: Theme.of(context).textTheme.bodyMedium),
-              const SizedBox(width: 16),
-              Image.asset('assets/images/desc2.png', height: 16),
-              const SizedBox(width: 4),
-              Text('$altitude m', style: Theme.of(context).textTheme.bodyMedium),
-              const SizedBox(width: 16),
-              Image.asset('assets/images/desc3.png', height: 16),
-              const SizedBox(width: 4),
-              Text('$speed m', style: Theme.of(context).textTheme.bodyMedium),
-            ],
-          ),
+          Image.asset("assets/images/$imagePath.png"),
           const SizedBox(height: 8.0),
-          Image.asset(imagePath),
+          Text('Distance: $distance km'),
+          Text('Altitude: $altitude m'),
+          Text('Vitesse: $speed km/h'),
         ],
       ),
     );
